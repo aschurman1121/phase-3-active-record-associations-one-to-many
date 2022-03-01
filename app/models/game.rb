6c422bf60ed52d7602e957236e78e40f36b3d303
alexandra <alexandraschurman@gmail.com>
2022-02-28 21:46:50 -0800

    commit tree d59b1b3ec62ab8109e7ee416afa8bf7ffc6bb8f5

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
   
+    # this method symbol is plural
+    has_many :reviews
+
+
+    # if we wanted to write this method out we would do somehting like this
+    # def reviews
+    #     Review.where(game_id: self.id)
+    #   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ActiveRecord::Base
-  
+    #review belongs to game
+#   def game
+#     #self = review instance
+#     Game.find(self.game_id)
+#   end
+
+    # the method below does the same thing as the method above
+    # for this method the :symbol must be singular
+    belongs_to :game
 end
